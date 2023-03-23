66f69c58007ddf3b7c4c21db1168a67415871693
vladimer.dolidze <Sazamtro123!>
2023-03-23 14:36:46 +0400

    device uuid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import DeviceUUID from 'device-uuid';
+import {useEffect} from "react";
 
 function App() {
+
+  var asd = DeviceUUID;
+
+  useEffect(() => {
+    alert(`your unique id is : ${asd.DeviceUUID().get()}`)
+  },[asd])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*{uuid}*/}
     </div>
   );
 }
